3a986ab92b0eb0395ca3bc635a5f460c361f8d1a
IvanovaNataly <letter4natalia@gmail.com>
2018-06-18 23:10:13 +0300

    sticky header with sticky column

--- scripts/vendors/responsive-table/responsive-tables.js
+++ scripts/vendors/responsive-table/responsive-tables.js
@@ -25,12 +25,24 @@ $(document).ready(function() {
 	{
 		original.wrap("<div class='table-wrapper' />");
 		
+
+
+
+		var header = original.clone();
+		header.find("tr:not(.table-header)").css("display", "none");
+		original.find("tr.table-header").css("display", "none");
+		header.removeClass("responsive");
+
+		original.closest(".table-wrapper").prepend(header);
+		header.wrap("<div class='mobile-header' />");
+
 		var copy = original.clone();
 		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
 		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
+
+		original.closest(".table-wrapper").prepend(copy);
 		copy.wrap("<div class='pinned' />");
+
 		original.wrap("<div class='scrollable' />");
 
     setCellHeights(original, copy);
